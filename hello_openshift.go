b7245717b47483f6200c15021f37ff9702805bf9
Shoubhik Bose <sbose78@gmail.com>
2018-11-04 23:56:52 +0530

    update response to API

--- hello_openshift.go
+++ hello_openshift.go
@@ -9,9 +9,8 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift!"
+		response = "Hello OpenShift Booster!"
 	}
-
 	fmt.Fprintln(w, response)
 	fmt.Println("Servicing request.")
 }
